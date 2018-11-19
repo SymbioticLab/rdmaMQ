9da368a6ef08e6d4c126fb7fa939227385b99797
Yiwen Zhang <yiwenzhg@umich.edu>
2018-11-18 22:21:04 -0500

    add comment for top-level module

--- src/rmq.h
+++ src/rmq.h
@@ -1,5 +1,12 @@
 #ifndef RMQ_H_
 #define RMQ_H_
 
+#include "config_rmq.h"
+
+/**
+ * rmq is the top-level model. It manages tranport, producer/consumer/brokers.
+ * Each application must first instantiate rmq.
+ */
+
 
 #endif // RMQ_H_
