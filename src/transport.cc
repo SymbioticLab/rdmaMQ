3dad233514da1a9a7186ec9eef6ed6fa6c716536
Yiwen Zhang <yiwenzhg@umich.edu>
2018-11-18 23:17:08 -0500

    implement transport::init() to open ibdev and alloc pd

---
+++ src/transport.cc
@@ -0,0 +1,23 @@
+#include "mbuf.h"
+
+namespace rmq {
+
+void Transport::init() {
+    int num_devices = 0;
+    struct ibv_device **dev_list = ibv_get_device_list(&num_devices);
+    assert_true(num_devices > 0, "Failed to find ib devices.");
+
+    for (int i = 0; i < num_devices; i++) {
+        ctx = ibv_open_device(dev_list[i]);
+        if (ctx) break;
+    }
+    assert_true(ctx != nullptr, "Failed to open ib device " + std::to_string(ibv_get_device_name(i)));
+
+    // TODO: (low priority) query device to pick the correct MTU
+    // TODO: (medium priority) create complete channel
+    pd = ibv_alloc_pd(ctx);
+    assert_true(pd != nullptr, "Failed to allocate protection domain.");
+}
+
+
+}
