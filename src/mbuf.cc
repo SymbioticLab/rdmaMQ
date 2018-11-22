d323789d66331179a3c2b80c2fe7be8ccfc25b54
Yiwen Zhang <yiwenzhg@umich.edu>
2018-11-22 15:33:34 -0500

    bug fix in mbuf.cc

--- src/mbuf.cc
+++ src/mbuf.cc
@@ -3,11 +3,14 @@
 
 namespace rmq {
 
-void MessageBuffer::init() {
-    addr = memalign(sysconf(_SC_PAGESIZE), total_size);
+template <typename T>
+void MessageBuffer<T>::init() {
+    //addr = memalign(sysconf(_SC_PAGESIZE), total_size);
+    addr = malloc(total_size);
     mr = ibv_reg_mr(pd, addr, total_size,
             IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_REMOTE_READ);
     assert_exit(mr, "Failed to register MR.");
+    LOG_DEBUG("Managed to register MR for mbuf.\n");
 }
 
 // explicit instantiations
