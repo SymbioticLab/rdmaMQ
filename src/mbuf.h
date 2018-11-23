070f7870e8328966667821b4a049fae5b17e8292
Yiwen Zhang <yiwenzhg@umich.edu>
2018-11-22 20:33:03 -0500

    include shared_ptr to class transport in mbuf to do reg_mr

--- src/mbuf.h
+++ src/mbuf.h
@@ -3,15 +3,19 @@
 
 #include <infiniband/verbs.h>
 //#include "rmq.h"
+#include "transport.h"
 #include "common.h"
 
 namespace rmq {
 
 /**
- * Applications use MessageBuffer to store data to avoid low-level memory management 
- * details in RDMA. The size of the buffer is fixed once declared. The buffer can 
- * contains multiple chunks, but is not circular. Users take the responsibility to 
+ * Applications use MessageBuffer to store data to avoid low-level memory management
+ * details in RDMA. The size of the buffer is fixed once declared. The buffer can
+ * contains multiple chunks, but is not circular. Users take the responsibility to
  * ensure the data is stored in the expected position in the buffer.
+ * 
+ * Transport object should have been constructed before construct a MessageBuffer
+ * object, or MR registration would fail due to the absense of a protection domain.
  */ 
 template <typename T>
 class MessageBuffer {
@@ -24,22 +28,23 @@ private:
     std::unique_ptr<T[]> data;     // fixed sized array
 
     // RDMA transport metadata
-    //Transport *transport;
+    std::shared_ptr<Transport> transport;
     struct ibv_mr *mr;
     
     /**
-     * register memory region for RDMA
+     * register a memory region for RDMA
      */
     void init();
 
 public:
     MessageBuffer() {}
-    MessageBuffer(size_t capacity)
+    MessageBuffer(size_t capacity, Transport *transport)
     : capacity(capacity),
       num_blocks(0),
       block_size(sizeof(T)),
       total_size(capacity * sizeof(T)),
-      data(nullptr) {
+      data(nullptr),
+      transport(transport) {
         init();
     }
     // TODO: implement destructor
