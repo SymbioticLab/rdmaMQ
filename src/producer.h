433f1dba3d276a86c9554a455f14484df3ce0740
Yiwen Zhang <yiwenzhg@umich.edu>
2018-11-28 01:30:44 -0500

    init transport in producer

--- src/producer.h
+++ src/producer.h
@@ -13,6 +13,10 @@ namespace rmq {
  * 
  * For simplicity, each producer only creates one buffer instead
  * of multiple with different types.
+ * 
+ * In the aspect of tranport, producer is a sender rather than
+ * receiver. User will pass in the ip of the receiver (in this
+ * case the broker) to set up RDMA connection.
  */
 
 template <typename T>
@@ -28,6 +32,9 @@ private:
 
 public:
     Producer() {}
+    Producer(char *broker_ip) : broker_ip(broker_ip) {
+        transport = std::make_unique<Transport>();
+    }
     ~Producer() {}
 
 
