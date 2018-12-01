4804d655a5fea19a768a5bdc60c915a5d7226658
Yiwen Zhang <yiwenzhg@umich.edu>
2018-11-30 23:56:40 -0500

    Makefile for app

---
+++ app/main.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "rmq.h"
+#include "mbuf.h"
+#include "transport.h"
+#include "producer.h"
+#include "broker.h"
+#include "consumer.h"
+
+int main() {
+	//auto transport = std::make_shared<rmq::Transport>();
+	//rmq::MessageBuffer<int> buffer(10, transport->get_pd());
+	auto producer = new rmq::Producer<int>(1000, "10.0.0.2");
+	std::cout << "hello world" << std::endl;
+	auto broker = new rmq::Broker<int>();
+
+	return 0;
+}
