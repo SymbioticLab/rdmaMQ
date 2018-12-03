1fc71a63169e4417f861b5b8dd191fe65682795d
Yiwen Zhang <yiwenzhg@umich.edu>
2018-12-02 22:22:54 -0500

    add a wait time after consumer init transport so producer can first write something

--- app/main.cc
+++ app/main.cc
@@ -10,13 +10,11 @@ void run_producer() {
     std::cout << "Running as Producer" << std::endl;
     auto producer = new rmq::Producer<int>(1000, "10.0.0.2");
     producer->init_transport();
-    /*
     for (int i = 0; i < 100; i++) {
         producer->data()[i] = i;
         producer->push(i, 1);
-        //std::cout << "Data: " << producer->data()[i] << std::endl;
+        std::cout << "Data: " << producer->data()[i] << std::endl;
     }
-    */
     while(1) {}
 }
 
@@ -24,6 +22,7 @@ void run_consumer() {
     std::cout << "Running as Consumer" << std::endl;
     auto consumer = new rmq::Consumer<int>(1000, "10.0.0.2");
     consumer->init_transport();
+    sleep(2);   // so that producer can write something
     std::cout << "remote write idx = " << consumer->fetch_write_idx() << std::endl;
     for (int i = 0; i < 100; i++) {
         consumer->pull(i, i, 1);
