24ea993cfe9fa418c3da16fbedd93afb4a1e5ef4
Yiwen Zhang <yiwenzhg@umich.edu>
2018-12-07 22:36:04 -0500

    Java Kafka producer microbenchmarking

--- kafka_java_examples/src/main/java/kafka/examples/Producer.java
+++ kafka_java_examples/src/main/java/kafka/examples/Producer.java
@@ -26,6 +26,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import java.lang.Math;
 
 public class Producer extends Thread {
     private final KafkaProducer<Integer, String> producer;
@@ -44,31 +45,42 @@ public class Producer extends Thread {
     }
 
     public void run() {
-        long messageNo = 1;
-        long NUM_REQ = 1000000;
-        long diff[] = new long[NUM_REQ];
+        int messageNo = 1;
+        int NUM_REQ = 100000;
+        long lat[] = new long[NUM_REQ];
         while (messageNo - 1 < NUM_REQ) {
         //while (true) {
-            //String messageStr = "Message_" + messageNo;
-            long startTime = System.currentTimeMillis();
+            String messageStr = "Message_" + messageNo;
             if (isAsync) { // Send asynchronously
+                long startTime = System.currentTimeMillis();
                 producer.send(new ProducerRecord<>(topic,
                     messageNo,
                     messageStr), new DemoCallBack(startTime, messageNo, messageStr));
             } else { // Send synchronously
                 try {
+                    long startTime = System.nanoTime();
                     producer.send(new ProducerRecord<>(topic,
                         messageNo,
                         messageStr)).get();
                     //System.out.println("Sent message: (" + messageNo + ", " + messageStr + ")");
+                    long elapsedTime = System.nanoTime() - startTime;
+                    //System.out.println("elapsedTime: " + elapsedTime);
+                    lat[messageNo-1] = elapsedTime;
                 } catch (InterruptedException | ExecutionException e) {
                     e.printStackTrace();
                 }
-                long elapsedTime = System.currentTimeMillis() - startTime;
-                diff[messageNo-1] = elapsedTime;
             }
             ++messageNo;
         }
+        int idx_m = (int)Math.ceil(NUM_REQ * 0.5);
+        int idx_99 = (int)Math.ceil(NUM_REQ * 0.99);
+        int idx_99_9 = (int)Math.ceil(NUM_REQ * 0.999);
+        int idx_99_99 = (int)Math.ceil(NUM_REQ * 0.9999);
+        System.out.println("@MEASUREMENT:");
+        System.out.println("MEDIAN = " + (double)lat[idx_m]/1000 + " us");
+        System.out.println("99 TAIL = " + (double)lat[idx_99]/1000 + " us");
+        System.out.println("99.9 TAIL = " + (double)lat[idx_99_9]/1000 + " us");
+        System.out.println("99.99 TAIL = " + (double)lat[idx_99_99]/1000 + " us");
     }
 }
 
@@ -95,6 +107,8 @@ class DemoCallBack implements Callback {
      */
     public void onCompletion(RecordMetadata metadata, Exception exception) {
         long elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println("elapsedTime: " + elapsedTime);
+        /*
         if (metadata != null) {
             System.out.println(
                 "message(" + key + ", " + message + ") sent to partition(" + metadata.partition() +
@@ -103,5 +117,6 @@ class DemoCallBack implements Callback {
         } else {
             exception.printStackTrace();
         }
+        */
     }
 }
